e43c93b78d41d31eb520844f14f27a8b029c9fad
novr (Nobuhisa Komiya) <n_komiya@yumemi.co.jp>
2024-03-09 00:41:32 +0900

    fix: example

---
+++ example/src/example.ts
@@ -0,0 +1,25 @@
+// example.ts
+import 'dotenv/config'
+
+import {
+  Configuration,
+  BuildsApi,
+} from '@novr/bitrise-api'
+
+async function runExample() {
+  // Set up the API client with your configuration
+  const config = new Configuration({
+    apiKey: process.env.API_KEY, // Update with your actual API key
+  })
+
+  try {
+    // Example: Get a list of apps
+    const buildsApi = new BuildsApi(config)
+    const buildsList = await buildsApi.buildListAll()
+    console.log('Builds List:', buildsList)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+runExample()

fd164d1cb851ccf6f4c32e3cb2503040cd98eaf1
novr (Nobuhisa Komiya) <n_komiya@yumemi.co.jp>
2024-03-09 00:29:00 +0900

    fix: dist

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     node: true,
   },
-  ignorePatterns: ['lib/', 'dist/'],
+  ignorePatterns: ['src/lib/', 'dist/'],
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   rules: {
     // 任意の eslint ルールを追加できます

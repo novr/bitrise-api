77300cc9a5ee010a63c87e5f031f9367e5a69ca9
novr (Nobuhisa Komiya) <n_komiya@yumemi.co.jp>
2024-03-08 23:48:37 +0900

    chore: format

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,29 +1,34 @@
 // index.ts
 
-import { BitriseApiClient, Configuration, UserApi, AppsApi } from 'bitrise-api';
+import {
+  BitriseApiClient,
+  Configuration,
+  UserApi,
+  AppsApi,
+} from '@novr/bitrise-api'
 
 async function runExample(apiKey: string) {
   // Set up the API client with your configuration
   const config = new Configuration({
     apiKey: apiKey, // Update with your actual API key
-  });
+  })
 
-  const apiClient = new BitriseApiClient(config);
+  const apiClient = new BitriseApiClient(config)
 
   try {
     // Example: Get information about the authenticated user
-    const userApi = new UserApi(apiClient);
-    const userInfo = await userApi.me();
-    console.log('User Information:', userInfo);
+    const userApi = new UserApi(apiClient)
+    const userInfo = await userApi.me()
+    console.log('User Information:', userInfo)
 
     // Example: Get a list of apps
-    const appsApi = new AppsApi(apiClient);
-    const appsList = await appsApi.listApps();
-    console.log('Apps List:', appsList);
+    const appsApi = new AppsApi(apiClient)
+    const appsList = await appsApi.listApps()
+    console.log('Apps List:', appsList)
   } catch (error) {
-    console.error('Error:', error.message || error);
+    console.error('Error:', error.message || error)
   }
 }
 
-const apiKey = 'your-api-key'; // 本物のAPIキーを使用するか、Bitriseダッシュボードから取得したテスト用のAPIキーを使用してください
-runExample(apiKey);
+const apiKey = 'your-api-key' // 本物のAPIキーを使用するか、Bitriseダッシュボードから取得したテスト用のAPIキーを使用してください
+runExample(apiKey)
